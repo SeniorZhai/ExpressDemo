84afedf7d190079ec66cc1d47ee1dc2328920c9a
仔仔 <370985116@qq.com>
2015-01-24 16:13:50 +0800

    修改路由

--- app.js
+++ app.js
@@ -1,60 +1,23 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+var app = express();                                    // 生成一个express实例
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));        // 设置视图文件夹
+app.set('view engine', 'ejs');                          // 设置模板引擎
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));                                 // 加载日志中间件
+app.use(bodyParser.json());                             // 加载解析json的中间件
+app.use(bodyParser.urlencoded({extended: true}));       // 加载解析urlencoded请求体的中间件
+app.use(cookieParser());                                // 加载cookie的中间件
+app.use(express.static(path.join(__dirname, 'public')));// 设置public文件夹为存放静态文件的目录
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
+console.log('Express服务已开启!');
 module.exports = app;
