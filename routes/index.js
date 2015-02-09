5637b582e84d816635cdd1a5303126faad7d4bdc
仔仔 <370985116@qq.com>
2015-02-09 13:51:59 +0800

    添加登出

--- routes/index.js
+++ routes/index.js
@@ -59,4 +59,34 @@ module.exports = function (app) {
             });
         });
     })
+    app.post('/login', function (req,res) {
+        var md5 = crypto.createHash('md5'),
+            password = md5.update(req.body.password).digest('hex');
+        User.get(req.body.name,function(err,user){
+            if (!user) {
+                req.flash('error','用户不存在');
+                return res.redirect('/login');  // 用户不存在则跳转到登陆页
+            }
+            if (user.password != password) {
+                req.flash('error','密码错误!');
+                return res.redirect('/login');
+            }
+            req.session.user = user;
+            req.flash('sucess','登陆成功!');
+            res.redirect('/');
+        });
+    });
+    app.get('/login',function(req,res) {
+       res.render('login',{
+           title:'登录',
+           user:req.session.user,
+           success:req.flash('success').toString(),
+           error:req.flash('error').toString()
+       });
+    });
+    app.get('/logout',function(req,res) {
+        req.session.user = null;
+        req.flash('success','登出成功');
+        res.redirect('/');  // 跳回主页
+    });
 };
